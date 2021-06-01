ac1081dd8a8fb4df3c77e4d3a7a739c3ee96798b
hussamm98 <hussam.zquad@gmail.com>
2021-06-01 18:05:10 +0200

    imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/HAM_Chat/welcome_chat_screen.dart';
-import 'package:flutter_auth/User_Interface/components/navigation_bar.dart';
-import 'package:flutter_auth/User_Interface/pages/category_screen.dart';
-import 'package:flutter_auth/User_Interface/pages/confirm_password.dart';
-import 'package:flutter_auth/User_Interface/pages/home.dart';
-import 'package:flutter_auth/User_Interface/pages/login_screen.dart';
-import 'package:flutter_auth/User_Interface/pages/profile_screen.dart';
 import 'package:flutter_auth/User_Interface/pages/welcome_screen.dart';
 import 'package:flutter_auth/constants.dart';
-import 'User_Interface/components/bottom_navigation.dart';
-import 'User_Interface/pages/schedule_screen.dart';
 
 void main() => runApp(MyApp());
 
