376b2453414a89d2888bd111f1c052e043cf21ab
hussamm98 <hussam.zquad@gmail.com>
2021-06-02 06:26:00 +0200

    some edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/User_Interface/pages/welcome_screen.dart';
 import 'package:flutter_auth/constants.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
